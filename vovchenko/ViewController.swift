423b5d19a39efe5258f6f414915dc2ed1547531f
student <student@novy-fort.ttknn.net>
2017-12-08 12:43:31 +0300

    in

--- vovchenko/ViewController.swift
+++ vovchenko/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var isLighton = true
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        isLighton = !isLighton
+        
+        if isLighton {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
